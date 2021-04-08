616506f4c1319cf18cccdcd2a35f967f4a617067
Viktory-me <73599864+Viktory-me@users.noreply.github.com>
2021-04-08 15:46:25 +0300

    вроде финиш.

--- hw.js
+++ hw.js
@@ -34,7 +34,6 @@ evt.preventDefault(); //прерывание перехода по ссылке!
 
 if (evt.target.localName === 'img'){
   
-
   refs.modalImg.src=evt.target.dataset.source;
   refs.modalImg.alt=evt.target.alt;
   refs.modalImg.dataset.index=evt.target.dataset.index;
@@ -63,8 +62,36 @@ const onKeyClick= evt=>{
 
 window.addEventListener('keyup',onKeyClick);
 
+// пролистывание влево-врпаво по стрелкам клавы
 
-// function newSrc(index, step = 0) {
-//   refs.modalImg.dataset.index = `${index + step}`;
-//   refs.modalImg.src = gallery[index + step].original;
-// };
+function newSrc(index, step = 0) {
+  refs.modalImg.dataset.index = `${index + step}`;
+  refs.modalImg.src = gallery[index + step].original;
+};
+
+function onArrowLeft() {
+  let index = +refs.modalImg.dataset.index;
+  if (index === 0) {
+    newSrc(gallery.length - 1);
+    return;
+  }
+  newSrc(index, -1);
+};
+
+function onArrowRight() {
+  let index = +refs.modalImg.dataset.index;
+  if (index === gallery.length - 1) {
+    newSrc(0);
+    return;
+  }
+  newSrc(index, 1);
+};
+
+window.addEventListener('keydown', evt=> {
+  if (evt.code === 'ArrowLeft') {
+    onArrowLeft();
+  }
+  if (evt.code === 'ArrowRight') {
+    onArrowRight();
+  }
+});
